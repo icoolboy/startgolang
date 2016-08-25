55ed9463b3c52a69c52d5465d07259eca1ff4875
Cool Boy <mr.akbarimanesh@gmail.com>
2016-08-25 21:48:00 +0430

    Create 01_install_environment_configuration.go

---
+++ 01_install_environment_configuration.go
@@ -0,0 +1,10 @@
+//Follow under steps for install and config go on your os 
+//This steps is general, Is not specific to a particular operating system
+1. Download Go Compiler for your os from https://golang.org/dl
+2. If compiler version is installer , Install that on your os or if compiler is archive version copy that on your target folder and exteract it
+3. Usally you need set path on environment variables, in this step you must create two variable (GOROOT, GOPATH) . in a normal no need to create GOROOT
+4. if you've install on os drive you don't need add installation location address to environment path else you must go to installation folder and copy address bar then add to end of "path" variable in environment variables
+5. create folder(workspace) on ideal drive and copy folder address then goto the environment variables and create "GOPATH" then set value equal to "[workspace folder address]"
+6. Apply changes and ok then close all cmd and reexecute cmd and enjoy from run go apps 
+
+Good Luck
