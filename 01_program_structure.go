d0da7b2e696848ba4e2e37ee8849cdbb5c187293
Cool Boy <mr.akbarimanesh@gmail.com>
2016-08-25 21:33:52 +0430

    Create 01_program_structure.go

---
+++ 01_program_structure.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+//main function for init
+func main() {
+	fmt.Println("IRAN GophersAcademy, dotgo.asia, daneshjooyar.com")
+}
+
+/*
+ Comment 1
+ Comment 2
+ Comment 3
+*/
